dac11f5c96b7ef82b47f33f03e05822cd900307f
Mike Hoskins <unixdoes@gmail.com>
2018-01-01 17:17:17 -0500

    middleware to find food based on location

--- middleware/index.js
+++ middleware/index.js
@@ -10,4 +10,21 @@ middleware.logRequest = function(req, res, next) {
     return next();
 }
 
+middleware.randomFood = function(latitude, longitude, callback) {
+    // var food = {
+    //     "latitude": latitude,
+    //     "longitude": longitude
+    // };
+    food = searchYelp(latitude, longitude);
+    callback(null, food);
+}
+
+function searchYelp(latitude, longitude) {
+    var food = {
+        "latitude": latitude,
+        "longitude": longitude
+    };
+    return food
+}
+
 module.exports = middleware;
