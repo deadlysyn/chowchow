2fa52330e75231b8d986a62120e45ff344f939ad
Mike Hoskins <unixdoes@gmail.com>
2018-01-03 20:53:31 -0500

    ui refactor

--- app.js
+++ app.js
@@ -16,43 +16,47 @@ app.get('/', middleware.logRequest, function(req, res) {
     res.render('index');
 });
 
-app.get('/food', middleware.logRequest, function(req, res) {
-    if (Object.keys(req.query).length === 0) {
-        res.redirect('/');
-    } else {
-        if (res.query.random == true) {
-            middleware.random(req.params.latitude, req.params.longitude, function(err, food) {
-                if (err != null) {
-                    console.log(err);
-                } else {
-                    res.render('random', {food: food});
-                }
-            });
-        } else {
-            middleware.list(req.params.latitude, req.params.longitude, function(err, food) {
-                if (err != null) {
-                    console.log(err);
-                } else {
-                    res.render('random', {food: food});
-                }
-            });
-        }
-    }
+app.post('/', middleware.logRequest, function(req, res) {
+    res.send(req.body);
 });
 
-// app.get('/random/:latitude/:longitude', middleware.logRequest, function(req, res) {
-//     middleware.randomFood(req.params.latitude, req.params.longitude, function(err, food) {
-//         if (err != null) {
-//             console.log(err);
+// app.get('/food', middleware.logRequest, function(req, res) {
+//     if (Object.keys(req.query).length === 0) {
+//         res.redirect('/');
+//     } else {
+//         if (res.query.random == true) {
+//             middleware.random(req.params.latitude, req.params.longitude, function(err, food) {
+//                 if (err != null) {
+//                     console.log(err);
+//                 } else {
+//                     res.render('random', {food: food});
+//                 }
+//             });
 //         } else {
-//             res.render('random', {food: food});
+//             middleware.list(req.params.latitude, req.params.longitude, function(err, food) {
+//                 if (err != null) {
+//                     console.log(err);
+//                 } else {
+//                     res.render('random', {food: food});
+//                 }
+//             });
 //         }
-//     });
+//     }
 // });
 
-// app.get('/list/:latitude/:longitude', middleware.logRequest, function(req, res) {
-//     res.render('list');
-// });
+app.get('/random/:latitude/:longitude', middleware.logRequest, function(req, res) {
+    middleware.randomFood(req.params.latitude, req.params.longitude, function(err, food) {
+        if (err != null) {
+            console.log(err);
+        } else {
+            res.render('random', {food: food});
+        }
+    });
+});
+
+app.get('/list/:latitude/:longitude', middleware.logRequest, function(req, res) {
+    res.render('list');
+});
 
 app.get('*', middleware.logRequest, function(req, res) {
     res.status(404).render('catchall');
