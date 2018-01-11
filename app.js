c978ebb041409c6ba64aa54321ffbe1a8952a0c7
Mike Hoskins <unixdoes@gmail.com>
2018-01-10 20:51:37 -0500

    debug internal server errors on safari

--- app.js
+++ app.js
@@ -74,6 +74,7 @@ app.post('/random', m.logRequest, m.parseRequest, function(req, res, next) {
 })
 
 app.get('/random', m.logRequest, function(req, res, next) {
+    // got random choice from yelp
     if (req.session.choice.name) {
         console.log('GET ROUTE: ' + req.session.choice.name)
         res.render('random', {biz: req.session.choice})
