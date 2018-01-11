cf7a31af749e0fc67295fef9594ca4bbdb5f52c1
Mike Hoskins <unixdoes@gmail.com>
2018-01-10 22:30:02 -0500

    debug internal server errors on safari

--- app.js
+++ app.js
@@ -74,6 +74,7 @@ app.get('/', m.logRequest, function(req, res, next) {
 app.post('/random', m.logRequest, m.parseRequest, function(req, res, next) {
     req.session.save(function(err) {
         if (err) {
+            console.log('ERROR: ' + err)
             res.redirect('/')
         } else {
             res.redirect('/random')
